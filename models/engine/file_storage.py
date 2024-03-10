7f02579cae3002243e671f6e2bb6d6e779886af3
0obito <elmsafer.abdellah00@gmail.com>
2024-03-10 18:34:14 +0000

    FileStorage file updates

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Module for file_storage.py
+"""
 
 import json
 from models.base_model import BaseModel
@@ -11,26 +14,51 @@ from models.state import State
 
 
 class FileStorage:
+    """
+    FileStorage class
+    Attributes:
+        __file_path: contains the path to
+            the json file we're manipulating
+        __objects: a dictionary that will contain
+            every all the objects
+    """
     __file_path = "file.json"
     __objects = {}
 
     def __init__(self):
+        """
+        The init method
+        """
         pass
 
     def all(self):
+        """
+        returns the dictionary __objects
+        """
         return FileStorage.__objects
 
     def new(self, obj):
+        """
+        sets in __objects the obj
+        with key <obj class name>.id
+        """
         variable = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[variable] = obj
 
     def save(self):
+        """
+        serializes __objects to
+        the JSON file (path: __file_path)
+        """
         with open(FileStorage.__file_path, 'w') as file:
             for key, value in FileStorage.__objects.items():
                 FileStorage.__objects[key] = value.to_dict()
             json.dump(FileStorage.__objects, file)
 
     def reload(self):
+        """
+        deserializes the JSON file to __objects
+        """
         try:
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
@@ -40,6 +68,4 @@ class FileStorage:
                     self.new(obj)
 
         except FileNotFoundError:
-            pass
-        except json.JSONDecodeError:
             pass
